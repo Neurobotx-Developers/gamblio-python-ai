77900f96694a52f4dd8bc766219f483effff16b8
tijanaradojicic <tijana.radojicic@neurobotx.me>
2024-11-05 11:50:31 +0100

    alembic initial migration file

---
+++ alembic/versions/f01688fd9278_create_ai_tables.py
@@ -0,0 +1,26 @@
+"""create_ai_tables
+
+Revision ID: f01688fd9278
+Revises: 
+Create Date: 2024-11-05 11:49:20.228877
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'f01688fd9278'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
