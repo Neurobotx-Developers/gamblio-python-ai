009fa710f6dce836b79c77926c0a86b1bf0e4f68
tijanaradojicic <tijana.radojicic@neurobotx.me>
2024-11-05 12:17:47 +0100

    configure alembic

--- database.py
+++ database.py
@@ -1,3 +1,13 @@
 from sqlalchemy import create_engine
+from sqlalchemy import URL
 
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+# TODO fill data from .env
+url_object = URL.create(
+    "postgresql+psycopg2",
+    username="",
+    password="",
+    host="",
+    database="",
+    port=5432
+)
+engine = create_engine(url_object, echo=True)
