e3933e5878d37bb7acd4e1c5aa9efc02faefa7d9
tijanaradojicic <tijana.radojicic@neurobotx.me>
2024-11-05 14:50:19 +0100

    migration for creating ai tables

--- alembic/versions/f01688fd9278_create_ai_tables.py
+++ alembic/versions/f01688fd9278_create_ai_tables.py
@@ -8,8 +8,7 @@ Create Date: 2024-11-05 11:49:20.228877
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
-
+from sqlalchemy.sql import text
 
 # revision identifiers, used by Alembic.
 revision: str = 'f01688fd9278'
@@ -19,27 +18,24 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    # Create Legacy baza table
-    op.create_table(
-        'legacy_baza',
-        sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('chat_id', sa.String(length=255), nullable=False),
-        sa.Column('message', sa.Text, nullable=False),
-        sa.Column('embedding', sa.LargeBinary, nullable=True)
-    )
-    # Create QA baza table
-    op.create_table(
-        'qa_baza',
-        sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('question', sa.Text, nullable=False),
-        sa.Column('answer', sa.Text, nullable=False),
-        sa.Column('question_embedding', sa.LargeBinary, nullable=True)
-    )
+    conn = op.get_bind()
+    query = """
+        CREATE TABLE legacy (
+            id SERIAL PRIMARY KEY,
+            chat_id INTEGER NOT NULL,
+            message TEXT NOT NULL,
+            embedding VECTOR
+        );
+        CREATE TABLE qa (
+            id SERIAL PRIMARY KEY,
+            question TEXT NOT NULL,
+            answer TEXT NOT NULL,
+            question_embedding VECTOR
+        );
+    """
+    conn.execute(text(query))
 
 
 def downgrade() -> None:
-    # Drop QA baza table
-    op.drop_table('qa_baza')
-
-    # Drop Legacy baza table
-    op.drop_table('legacy_baza')
+    op.drop_table('legacy')
+    op.drop_table('qa')
