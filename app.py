6b9a159d080f301ac20f34ca0134eb07036346e2
vasilijev7 <vasilijev7@gmail.com>
2024-11-07 12:21:43 +0100

    added ai functions

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 from time import sleep
 import time
 import os
+import tracemalloc
 
-
+tracemalloc.start()
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
@@ -18,7 +19,7 @@ client = OpenAI(api_key=api_key)
 global_assistant={}
 
 def create_assistant():
-    file_paths = ["knowlege_files/admiral-bet.txt","knowlege_files/tipovi-igara.txt"]
+    file_paths = ["knowlege_files/admiral-bet.txt", "knowlege_files/tipovi-igara.txt"]
     file_ids = []
     for path in file_paths:
         with open(path, "rb") as f:
@@ -27,14 +28,16 @@ def create_assistant():
                 purpose="assistants"
             )
         file_ids.append(file_response.id)
-        vector_store = client.beta.vector_stores.create(
-             name="WASCO AI",
-            file_ids=file_ids
-        )
+    
+    vector_store = client.beta.vector_stores.create(
+        name="WASCO AI",
+        file_ids=file_ids
+    )
+    
     assistant = client.beta.assistants.create(
         name="Wasco",
-        instructions= "Vi ste korisnički asistent za Admiral Bet Crna Gora, valuta je evro i govorite ijekavicom. Vaš zadatak je da promovišete Admiral Bet i njegove igre i iskljucivo odgovarate na pitanja vezana za Admiral Bet i njegove igre, te da pomažete korisnicima kao da ste stvarna osoba iz korisničke podrške. Nikada ne pominjete druge kladionice. Json odgovor",
-        tools =[{"type": "file_search" }],
+        instructions="Vi ste korisnički asistent za Admiral Bet Crna Gora, valuta je evro i govorite ijekavicom. Vaš zadatak je da promovišete Admiral Bet i njegove igre i iskljucivo odgovarate na pitanja vezana za Admiral Bet i njegove igre, te da pomažete korisnicima kao da ste stvarna osoba iz korisničke podrške. Nikada ne pominjete druge kladionice. Json odgovor",
+        tools=[{"type": "file_search"}],
         model="gpt-4o-mini",
         tool_resources={
             "file_search": {
@@ -42,17 +45,18 @@ def create_assistant():
             }
         }
     )
+    
     try:
-        client.beta.assistants.update(assistant.id, {
-            "tool_resources": { "file_search": { "vector_store_ids": [vector_store.id] } },
-        })
+        # No need for an extra argument, removing it from this update call
+        client.beta.assistants.update(assistant.id)
     except Exception as error:
         print("Failed to create assistant:", error)
+    
     return assistant
-
 def create_thread():
 
     thread = client.beta.threads.create(
+        
         messages= [
         {
           "role": "user",
@@ -84,12 +88,12 @@ def add_message_to_thread(thread_id, role, content):
     )
     return message
 
-def run_assistant(thread_id, assistant_id):
+def run_assistant(thread_id, assistant_id, instructions):
     run = client.beta.threads.runs.create(
         thread_id=thread_id,
         assistant_id=assistant_id,
         additional_instructions="Daj kratke i precizne odgovore. Koristi ijekavski dijalekt crnogorskog jezika. Nikada ne pominji fajl znanja.",
-        instructions= f"{global_assistant.instructions} + 'Format odgovora u JSON-u: {{\"sure\":true - ako si siguran da mozes dati validan odgovor. / false - ako mislis da na pitanje treba odgovoriti korisnicka podrska, \"answer\":\"\" - ako mislis da na to pitanje treba odgovoriti korisnicka podrska/\"ili neki tekst ukoliko si nasao odgovor u bazi znanja\"}} --- UVJEK ISTRAZI FAJL ZNANJA PRIJE NEGO ODGOVORIS'",
+        instructions= f"{instructions} + 'Format odgovora u JSON-u: {{\"sure\":true - ako si siguran da mozes dati validan odgovor. / false - ako mislis da na pitanje treba odgovoriti korisnicka podrska, \"answer\":\"\" - ako mislis da na to pitanje treba odgovoriti korisnicka podrska/\"ili neki tekst ukoliko si nasao odgovor u bazi znanja\"}} --- UVJEK ISTRAZI FAJL ZNANJA PRIJE NEGO ODGOVORIS'",
     )   
     return run
 
--- database.py
+++ database.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy import URL
-
+import os
 # TODO fill data from .env
 url_object = URL.create(
-    "postgresql+psycopg2",
-    username="",
-    password="",
-    host="",
-    database="",
+    "postgresql+pg8000",
+    username=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
     port=5432
 )
 engine = create_engine(url_object, echo=True)
