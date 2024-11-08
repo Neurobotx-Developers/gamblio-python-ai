53463241aebc307b746697f56e59e1dce5861ab8
haris-neurobotx <haris.sabotic@neurobotx.me>
2024-11-08 12:15:17 +0100

    Collect chat_id from arguments

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import asyncio
 from websocket_connection import connect_and_communicate
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--chat_id", type=int, help="ID of the chat")
+
+args = parser.parse_args()
 
 
-def start(chat_id):
-    asyncio.get_event_loop().run_until_complete(connect_and_communicate(chat_id))
+asyncio.get_event_loop().run_until_complete(connect_and_communicate(args.chat_id))
