e2b678b7d05f06372d1221d8a2bdc5c174277a4f
vasilijev7 <vasilijev7@gmail.com>
2024-11-19 15:32:21 +0100

    testing

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import asyncio
 from websocket_connection import connect_and_communicate
 import argparse
+import sentry_sdk
 
+sentry_sdk.init(
+    dsn="https://37cac10e14a03768e2d7a64225aa7c6e@o4508318885019648.ingest.de.sentry.io/4508324683776080",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+)
+
+# Manually call start_profiler and stop_profiler
+# to profile the code in between
+sentry_sdk.profiler.start_profiler()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--chat_id", type=int, help="ID of the chat")
--- websocket_connection.py
+++ websocket_connection.py
@@ -2,18 +2,7 @@ import json
 from config import CONFIG
 import websockets
 from websocket import create_connection
-import sentry_sdk
 
-sentry_sdk.init(
-    dsn="https://37cac10e14a03768e2d7a64225aa7c6e@o4508318885019648.ingest.de.sentry.io/4508324683776080",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
-    traces_sample_rate=1.0,
-)
-
-# Manually call start_profiler and stop_profiler
-# to profile the code in between
-sentry_sdk.profiler.start_profiler()
 
 async def send(websocket, message):
     await websocket.send(json.dumps(message))
@@ -28,18 +17,36 @@ async def receive(websocket):
 
 
 async def connect_and_communicate(chat_id):
-    try:
-        async with websockets.connect(CONFIG["WEBSOCKET_URL"]) as django_websocket:
-            await send(django_websocket, {"tag": "bot_subscribe", "chat_id": chat_id})
-            daemonUri = "ws://localhost:8765"
-            async with websockets.connect(daemonUri) as daemon_websocket:
-                while True:
-                    received_django = await receive(django_websocket)
-                    if "text" not in received_django:
-                        continue
-                    question = received_django["text"]
-                    await daemon_websocket.send(json.dumps({"question": question, "chat_id": chat_id}))
-                    daemon_response = await receive(daemon_websocket)
-                    # Handle the response as needed
-    except Exception as e:
-        print(f"Error in connect_and_communicate: {e}")
+    async with websockets.connect(CONFIG["WEBSOCKET_URL"]) as django_websocket:
+        await send(django_websocket, {"tag": "bot_subscribe", "chat_id": chat_id})
+        daemonUri = "ws://localhost:8765"
+        daemon_websocket = create_connection(daemonUri)
+        print("Connected to Daemon server")
+
+        while True:
+            received_django = await receive(django_websocket)
+            if not "text" in received_django:
+                continue
+
+            question = received_django["text"]
+
+            daemon_websocket.send(json.dumps({"question": question, "chat_id":chat_id}))
+            daemon_response = json.loads(daemon_websocket.recv())
+
+            if daemon_response["data"]["sure"] == False:
+                await send(
+                    django_websocket, {"tag": "bot_cannot_answer", "chat_id": chat_id}
+                )
+
+                break
+
+            await send(
+                django_websocket,
+                {
+                    "tag": "bot_send",
+                    "chat_id": chat_id,
+                    "text": daemon_response["data"]["answer"],
+                    "source": daemon_response["source"],
+                    "cost": daemon_response["cost"],
+                },
+            )
