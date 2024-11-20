7122ec6074d77d3dfa63cf10c185d183177066a0
vasilijev7 <vasilijev7@gmail.com>
2024-11-20 12:12:08 +0100

    debug

--- daemon.py
+++ daemon.py
@@ -66,10 +66,9 @@ def reformat_answer(answer, chat_id):
     
     # Convert to array of objects with content and role
     messages_array = []
-    for row in messages_rows:
-        messages_array.append({"content": row[0], "role": row[1]})  # Assuming role is in the second column
-    print("Legacy:", messages_array)  # Debug print
-    
+    messages_array = [{"content": row[0], "role": row[1]} for row in messages_rows]  # Create an array of objects
+    print("Chatovi:", messages_array)  # Debug print
+   
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
@@ -198,9 +197,8 @@ def get_openai_response(question, chat_id):
     messages_rows = CHAT_DB_CONNECTION.execute(query, {"id": chat_id}).fetchall()
     print(f"message rows: {messages_rows}")
     # Convert to array of objects with content and role
-    messages_array = []
-    for row in messages_rows:
-        messages_array.append({"content": row[0], "role": row[1]})  # Assuming role is in the second column
+    messages_array = [{"content": row[0], "role": row[1]} for row in messages_rows]  # Create an array of objects
+
     print("Chatovi:", messages_array)  # Debug print
     print("Legacy:", knowledge)
     add_message_to_thread(thread.id, "user", question, knowledge, messages_array)
