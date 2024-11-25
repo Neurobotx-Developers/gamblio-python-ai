cfa900883ec37e0bfae12e781eb42f3f127aa524
vasilijev7 <vasilijev7@gmail.com>
2024-11-25 13:38:26 +0100

    Dodata funkcija za upis podataka u QA kada AI odgovori

--- add_qa_to_database.py
+++ add_qa_to_database.py
@@ -4,7 +4,7 @@ def add_qa_to_database(question, answer):
     url = "https://gamblio-back.neurobotx.dev/api/knowledge/add-qa/"
     body = {
         "question": question,
-        "answer": answer
+        "answer": answer.answer
     }
     response = requests.post(url, json=body)
     return response.json()  # Vraća odgovor u JSON formatu
