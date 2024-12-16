2ba7301fd479fba233c5e2862ecdfffcde0a881a
vasilijev7 <vasilijev7@gmail.com>
2024-12-16 10:26:00 +0100

    DB vector bug fixing

--- daemon.py
+++ daemon.py
@@ -36,7 +36,7 @@ def search_qa_table(question):
         f"SELECT question, answer FROM qa WHERE question_embedding <-> '{embedding}' < :similarity ORDER BY question_embedding <-> '{embedding}';"
     )
 
-    result = DB_CONNECTION.execute(query, {"similarity": 0.5})
+    result = DB_CONNECTION.execute(query, {"similarity": 0.6})
 
     rows = result.fetchall()
 
